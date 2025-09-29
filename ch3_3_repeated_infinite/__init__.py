3c2c7bbec19e6693647dd1e33cd505d94dcc3570
yshimod <55014385+yshimod@users.noreply.github.com>
2025-09-29 17:02:33 +0900

    refactor ch3_3

--- ch3_3_repeated_infinite/__init__.py
+++ ch3_3_repeated_infinite/__init__.py
@@ -269,7 +269,7 @@ class Question(Page):
     form_fields = ["q1", "q2"]
 
 
-class keisanWaitPage(WaitPage):
+class KeisanWaitPage(WaitPage):
     wait_for_all_groups = True
     after_all_players_arrive = keisans
 
@@ -364,7 +364,7 @@ page_sequence = [
     Introduction,
     Decision,
     # Question,
-    keisanWaitPage,
+    KeisanWaitPage,
     ResultsWaitPage,
     # PreResults,
     Results,
